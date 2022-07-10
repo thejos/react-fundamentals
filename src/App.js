99de4d07c61d9ce284632fdfeefab5102a62f368
dejan.smiljic <dej4n.s@gmail.com>
2022-07-10 22:08:37 +0200

    Add 'Meet Another React Component' chapter code

--- src/App.js
+++ src/App.js
@@ -51,15 +51,23 @@ const itemArray = [
 /**1. This React component, called the App component, is just a JavaScript function. In contrast
 to JavaScript functions, it’s defined in PascalCase. This kind of component is commonly
 called a function component. Function components are the modern way of using components
-in React, however, be aware that there are other variations of React components too. 
-2. The App component doesn’t receive any parameters in its function signature yet. In
+in React, however, be aware that there are other variations of React components too.
+2. React application consists of many hierarchical components.
+React applications have component hierarchies (also called component trees). There is usually one
+uppermost entry point component (e.g. App) that spans a tree of components below it. The App
+is the parent component of the List and Search, so the List and Search are child components of
+the App component and sibling components to each other.
+In a component tree, there is always a root component (e.g. App), and the components that 
+don’t render any other components are called leaf components (e.g. List/Search component in our
+current source code). All components can have zero, one, or many child components.
+3. The App component doesn’t receive any parameters in its function signature yet. In
 the upcoming sections, you will learn how to pass information (props) from one component to
 another component. These props will be accessible via the function’s signature as parameters
 then.
 3. the App component returns code that resembles HTML. You will see how this new
 syntax, called JSX, allows you to combine JavaScript and HTML for displaying highly dynamic
 and interactive content in a browser.
-4. Comments inside react component need to be multi-line comment wrapped in curly braces*/
+5. Comments inside react component need to be multi-line comment wrapped in curly braces.*/
 function App() {
   return (
     <div>
@@ -74,24 +82,44 @@ function App() {
       </h3>
       <h3>hey {getTitle(title)} again!</h3>
       <hr />
+      {/*new Items component can be used in the App component where we have been
+      using the inlined functionality previously */}
+      <Items />
+      <Search />
+    </div>
+  );
+}
 
-      {/*use the built-in JavaScript map method for arrays to iterate over each item 
+/**Components are the foundation of every React application. Components should scale with application’s
+size. Instead of making one component larger and more complex, split one component into multiple 
+components eventually. Components encapsulate meaningful tasks while contributing to the greater good of a larger React
+application. Extracting a component is a task that should be performed very often. It’s always the case 
+that a component will grow in size and complexity*/
+function Items() {
+  return (
+    /*use the built-in JavaScript map method for arrays to iterate over each item 
       of the array (itemArray) and return each item's title.
-      Within the map function, we have access to each item and its properties. */}
-      <ul>
-        {itemArray.map(function (item) {
-          return (
-            <li key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>&ensp;- {item.author},</span>
-              <span> {item.points} pts,</span>
-              <span> {item.num_comments} comments</span>
-            </li>
-          );
-        })}
-      </ul>
+      Within the map function, we have access to each item and its properties. */
+    <ul>
+      {itemArray.map(function (item) {
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>&ensp;- {item.author},</span>
+            <span> {item.points} pts,</span>
+            <span> {item.num_comments} comments</span>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+function Search() {
+  return (
+    <div>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
     </div>
