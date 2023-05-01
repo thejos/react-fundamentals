d9b10ce9d74a9510f784e7eee2dd4b86d2408f02
Dejan Smiljić <dej4n.s@gmail.com>
2023-05-01 13:42:49 +0200

    Add "Testing in React: Test Suites, Test Cases, and Assertions" chapter code

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("something truthy and falsy", () => {
+  test("true to be true", () => {
+    expect(true).toBeTruthy();
+  });
+  test("false to be false", () => {
+    expect(false).toBeFalsy();
+  });
 });
